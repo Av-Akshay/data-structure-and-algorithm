5c8b0ec45e3dabf4ad71aadfc32e59a1b5033c09
Av-Akshay <akshuchauhan1jan@gmail.com>
2023-10-05 10:00:34 -0700

    Map data type

--- index.js
+++ index.js
@@ -34,15 +34,32 @@
 
 // video 31:- Object (hash table) =======================================================================
 
-let obj = {
-  name: "akshay chauhan",
-  age: 23,
-  email: "user@gmail.com",
-};
+// let obj = {
+//   name: "akshay chauhan",
+//   age: 23,
+//   email: "user@gmail.com",
+// };
 // for (item in obj) {
 //   console.log(item); // key
 //   console.log(obj[item]); // values
 // }
 
 // [console.log(Object.keys(obj))]; // array of keys
-[console.log(Object.values(obj))]; // array of values
+// [console.log(Object.values(obj))]; // array of values
+
+// video :- 32 Map data structure in javascript============================================================
+
+let map = new Map([[true, "vishal"]]);
+// or
+console.log(map);
+
+// const map = new Map();
+// map.set("name", "akshay");
+// map.set("age", 23);
+// map.set({ name: "saksham" }, "chauhan");
+// console.log(map.get("name"));
+// console.log(map.has("name"));
+// map.clear();
+// console.log(map);
+
+// this data tyep is simillar to object
